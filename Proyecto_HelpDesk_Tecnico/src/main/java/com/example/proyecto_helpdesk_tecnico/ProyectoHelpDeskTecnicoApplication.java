18d2d55397a07ef5b29e44d887fe6d322b5f5469
JW Sanchez <u21216300@utp.edu.pe>
2023-05-02 01:20:22 -0500

    Proyecto creado

---
+++ Proyecto_HelpDesk_Tecnico/src/main/java/com/example/proyecto_helpdesk_tecnico/ProyectoHelpDeskTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_helpdesk_tecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHelpDeskTecnicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoHelpDeskTecnicoApplication.class, args);
+    }
+
+}
---
+++ Proyecto_HelpDesk_Tecnico/src/test/java/com/example/proyecto_helpdesk_tecnico/ProyectoHelpDeskTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_helpdesk_tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHelpDeskTecnicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
